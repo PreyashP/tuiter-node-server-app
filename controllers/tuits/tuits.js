fec283269cb65d13655868d30deeab423b7e8e38
Preyash Patel <preyash8599@gmail.com>
2023-04-02 20:47:59 -0400

    A8 Tuiter Server Node

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        "_id": 123,
+        "topic": "Space",
+        "userName": "NASA",
+        "title": "NASA rover lands on Mars and discovers water",
+        "time": "2h",
+        "image": "nasa.webp",
+        "liked": true,
+        "disliked" : false,
+        "replies": 123,
+        "retuits": 3334,
+        "likes": 5345,
+        "dislikes": 99,
+        "handle": "@nasa",
+        "tuit": "NASA rover discovers water on Mars! We are looking into more sources of water on Mars as we follow the traces of this discovery."
+    },
+    {
+        "_id": 234,
+        "topic": "Travel",
+        "userName": "Virgin Galactic",
+        "time": "2h",
+        "title": "Hundreds of flights cancelled during winter storm",
+        "image": "virgin-galactic.png",
+        "liked": false,
+        "disliked": true,
+        "replies": 123,
+        "retuits": 456,
+        "likes": 2345,
+        "dislikes": 85,
+        "handle": "@virgingalactic",
+        "tuit": "Winter storm causing numerous cancellations across the country."
+    },
+
+    {
+        "_id": 345,
+        "topic": "Energy",
+        "userName": "NASA",
+        "title": "NASA develops new efficient rocket fuel",
+        "time": "6h",
+        "image": "nasa.webp",
+        "liked": true,
+        "disliked": false,
+        "replies": 321,
+        "retuits": 164,
+        "likes": 1234,
+        "dislikes": 76,
+        "handle": "@nasa",
+        "tuit": "NASA develops a new type of efficient rocket fuel. It is more cost effective and safer for the environment than the previous fuel."
+    }
+];
